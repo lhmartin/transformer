856b590ed942b862371a5ff4db776758e9d5d8a6
Luke Martin <lukechile@gmail.com>
2024-02-25 10:41:02 -0500

    fix pos embeddings

--- modules/positional_encoding.py
+++ modules/positional_encoding.py
@@ -1,4 +1,4 @@
-from torch import sin, cos, arange, stack, Tensor
+from torch import sin, cos, arange, zeros, Tensor, pow
 
 import torch.nn as nn
 
@@ -9,19 +9,18 @@ class SinCosPositionalEmbedding(nn.Module):
         self.model_dimension = model_dimension
         self.max_sequence_length = max_sequence_length
 
-        pos_vector = arange(0, max_sequence_length).unsqueeze(0)
-        denom = 10000 ** ((2 * arange(0, model_dimension)) / model_dimension).unsqueeze(0)
+        pos_vector = arange(0, max_sequence_length).unsqueeze(1)
+        denom = pow(10000.0, -arange(0,model_dimension,2)/model_dimension)
 
         # get only even rows
-        sin_embed = sin(pos_vector / denom.T)[0::2]
+        sin_embed = sin(pos_vector * denom)
 
         # get only odd rows
-        cos_embed = cos(pos_vector / denom.T)[1::2]
+        cos_embed = cos(pos_vector * denom)
 
-        # interleave rows
-        pos_embedings = stack((sin_embed, cos_embed), dim=1).view(
-            model_dimension, max_sequence_length
-        )
+        pos_embedings = zeros(max_sequence_length, model_dimension)
+        pos_embedings[:, 0::2] = sin_embed
+        pos_embedings[:, 1::2] = cos_embed
 
         # make sure the values are not optimized
         self.register_buffer("pos_embedings", pos_embedings)
